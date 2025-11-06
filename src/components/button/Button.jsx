5e629fff4bd08c38bd79fc03d5d71b5c869fc490
AngelesVasquezU <maria.vasquez.pineda@ucsp.edu.pe>
2025-11-06 18:44:02 -0500

    dashboard

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,12 +1,13 @@
 import "./Button.css";
 
-const Button = ({ children, onClick, type = "button", variant = "primary", disabled }) => {
+const Button = ({ children, onClick, type = "button", mt = "10px", variant = "primary", disabled }) => {
     return (
         <button
             className={`btn ${variant}`}
             onClick={onClick}
             type={type}
             disabled={disabled}
+            style={{ marginTop: mt }}
         >
             {children}
         </button>
